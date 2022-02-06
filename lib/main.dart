7edc5b0fd26fb6ff2638d2543120185975fb5ff1
Minh Ha <mihado@minh.im>
2022-02-06 16:54:56 +1300

    convert FlatButton to TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -33,55 +33,51 @@ class _QuizPageState extends State<QuizPage> {
       children: <Widget>[
         Expanded(
           flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
-                ),
+          child: Center(
+            child: Text(
+              'This is where the question text will go.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.white,
               ),
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: TextButton(
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.only(top: 20.00, bottom: 20.00),
+                backgroundColor: Colors.green),
+            child: Text(
+              'True',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
               ),
-              onPressed: () {
-                //The user picked true.
-              },
             ),
+            onPressed: () {
+              //The user picked true.
+            },
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: TextButton(
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.only(top: 20.00, bottom: 20.00),
+                backgroundColor: Colors.red),
+            child: Text(
+              'False',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
               ),
-              onPressed: () {
-                //The user picked false.
-              },
             ),
+            onPressed: () {
+              //The user picked false.
+            },
           ),
         ),
         //TODO: Add a Row here as your score keeper
